2e1a8dd0c132a367674d65a4b911c5926488b13a
김준성 <anima94@naver.com>
2019-06-27 16:25:29 +0900

    버튼까지만 잠시 가지고 놀기 이후부터는 영화보고와서

--- app/src/main/java/com/example/user/example1/MainActivity.java
+++ app/src/main/java/com/example/user/example1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.user.example1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onbutton1clicked(View v){
+        Toast.makeText(getApplicationContext(), "시작버튼이 눌렷어요", Toast.LENGTH_SHORT).show();
     }
 }
