282c2650e75e1f833443cdd8677e26e190e79a15
김준성 <anima94@naver.com>
2020-05-05 00:53:51 +0900

    Spinner,AutoCompleteTextView,ProgressBar,SeekBarv :airplane:

---
+++ Spinner,AutoCompleteTextView,ProgressBar,SeekBar/app/src/main/java/junseong/k/myapplication/MainActivity.java
@@ -0,0 +1,44 @@
+package junseong.k.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+    ProgressBar progressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Spinner spinner=findViewById(R.id.spinner);
+        String[] datas=getResources().getStringArray(R.array.spinner_array);
+        ArrayAdapter<String> aa=new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,datas);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(aa);
+
+        AutoCompleteTextView autoCompleteTextView=findViewById(R.id.auto);
+        String[] autodatas=getResources().getStringArray(R.array.auto_array);
+        ArrayAdapter<String> autoAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,autodatas);
+        autoCompleteTextView.setAdapter(autoAdapter);
+
+        progressBar=findViewById(R.id.progress);
+        ProgressThread thread=new ProgressThread();
+        thread.start();
+    }
+    class ProgressThread extends Thread{
+        @Override
+        public void run(){
+            for(int i=0; i<10; i++)
+            {
+                SystemClock.sleep(1000);
+                progressBar.incrementProgressBy(10);
+                progressBar.incrementSecondaryProgressBy(15);
+            }
+        }
+    }
+}
